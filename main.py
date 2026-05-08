631bee2d0d6d1494f060d4a6cf91f4247e5c37f7
Shravani M <mahadeshwarshravani18@gmail.com>
2026-05-08 16:29:54 +0200

    updated self play to 10_000

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ sys.path.insert(0, '.')
 from agents.random_agent import RandomAgent
 from agents.rule_agent import RuleAgent
 from training.self_play import run_self_play
-
+max=10_000 #no of games to run for each matchup
 print("=== 1. Random vs Random ===")
 run_self_play(RandomAgent("Random-A"), RandomAgent("Random-B"),
-              num_games=500, save_logs=False, save_dataset=False, base_seed=0)
+              num_games=max, save_logs=False, save_dataset=False, base_seed=0)
 
 print("\n=== 2. Rule vs Random ===")
 run_self_play(RuleAgent("Rule"), RandomAgent("Random"),
-              num_games=500, save_logs=False, save_dataset=False, base_seed=0)
+              num_games=max, save_logs=False, save_dataset=False, base_seed=0)
 
 print("\n=== 3. Rule vs Rule ===")
 run_self_play(RuleAgent("Rule-A"), RuleAgent("Rule-B"),
-              num_games=500, save_logs=False, save_dataset=True, base_seed=0)
+              num_games=max, save_logs=False, save_dataset=True, base_seed=0)
