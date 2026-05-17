6e51b3c2e50b25f4392589a21eceb1c8d14bf402
Shravani M <mahadeshwarshravani18@gmail.com>
2026-05-17 02:17:43 +0200

    updated match ups

--- main.py
+++ main.py
@@ -11,105 +11,50 @@ from training.self_play import run_self_play
 from config import Paths
 
 max = 10_000  # no of games to run for each matchup
+
 print("=== 1. Random vs Random ===")
-run_self_play(
-    RandomAgent("Random-A"),
-    RandomAgent("Random-B"),
-    num_games=max,
-    save_logs=False,
-    save_dataset=False,
-    base_seed=0,
-)
+run_self_play(RandomAgent("Random-A"),RandomAgent("Random-B"),
+              num_games=max,save_logs=False,save_dataset=False,base_seed=0,)
 
 print("\n=== 2. Rule vs Random ===")
-run_self_play(
-    RuleAgent("Rule"),
-    RandomAgent("Random"),
-    num_games=max,
-    save_logs=False,
-    save_dataset=False,
-    base_seed=0,
-)
+run_self_play(RuleAgent("Rule"),RandomAgent("Random"),
+            num_games=max,save_logs=False,save_dataset=False,base_seed=0,)
 
 print("\n=== 3. Rule vs Rule ===")
-run_self_play(
-    RuleAgent("Rule-A"),
-    RuleAgent("Rule-B"),
-    num_games=max,
-    save_logs=False,
-    save_dataset=False,
-    base_seed=0,
-)
+run_self_play(RuleAgent("Rule-A"),RuleAgent("Rule-B"),
+             num_games=max,save_logs=False,save_dataset=False,base_seed=0,)
 
 print("\n=== 4. Random vs Rule ===")
-run_self_play(
-    RandomAgent("Random"),
-    RuleAgent("Rule"),
-    num_games=max,
-    save_logs=False,
-    save_dataset=False,
-    base_seed=0,
-)
+run_self_play(RandomAgent("Random"),RuleAgent("Rule"),
+              num_games=max,save_logs=False, save_dataset=False,base_seed=0,)
 
 print("\n=== 5. Rule Good vs Rule Bad ===")
-run_self_play(
-    RuleAgent("Rule-Good"),
-    RuleBadAgent("Rule-Bad"),
-    num_games=max,
-    save_logs=False,
-    save_dataset=False,
-    base_seed=0,
-)
+run_self_play(RuleAgent("Rule-Good"),RuleBadAgent("Rule-Bad"),
+              num_games=max,save_logs=False,save_dataset=False,base_seed=0,)
 
 print("\n=== 6. Rule Bad vs Rule Good ===")
-run_self_play(
-    RuleBadAgent("Rule-Bad"),
-    RuleAgent("Rule-Good"),
-    num_games=max,
-    save_logs=False,
-    save_dataset=False,
-    base_seed=0,
-)
+run_self_play(RuleBadAgent("Rule-Bad"),RuleAgent("Rule-Good"),
+              num_games=max,save_logs=False,save_dataset=False,base_seed=0,)
 
 rl = RLAgent("RL", epsilon=0.0)
 rl.load(str(Paths.MODELS / "rl_agent.pt"))
 
 print("\n=== 7. RL vs Random ===")
-run_self_play(
-    rl,
-    RandomAgent("Random"),
-    num_games=max,
-    save_logs=False,
-    save_dataset=False,
-    base_seed=0,
-)
+run_self_play(rl,RandomAgent("Random"),
+              num_games=max,save_logs=False,save_dataset=False,base_seed=0,)
 
 print("\n=== 8. RL vs Rule ===")
-run_self_play(
-    rl,
-    RuleAgent("Rule"),
-    num_games=max,
-    save_logs=False,
-    save_dataset=False,
-    base_seed=0,
-)
+run_self_play(rl,RuleAgent("Rule"),
+    num_games=max,save_logs=False,save_dataset=False,base_seed=0,)
+
+print("\n=== 8. RL vs MCTS ===")
+run_self_play(rl,MCTSAgent("MCTS"),num_games=max,save_logs=False,save_dataset=False,base_seed=0,)
 
 print("\n=== 9. MCTS vs Random ===")
-run_self_play(
-    MCTSAgent("MCTS"),
-    RandomAgent("Random"),
-    num_games=10_000,
-    save_logs=False,
-    save_dataset=False,
-    base_seed=0,
-)
+run_self_play(MCTSAgent("MCTS"),RandomAgent("Random"),
+              num_games=10_000,save_logs=False,save_dataset=False,base_seed=0,)
 
 print("\n=== 10. MCTS vs Rule ===")
-run_self_play(
-    MCTSAgent("MCTS"),
-    RuleAgent("Rule"),
-    num_games=10_000,
-    save_logs=False,
-    save_dataset=False,
-    base_seed=0,
-)
+run_self_play(MCTSAgent("MCTS"),RuleAgent("Rule"),
+    num_games=10_000,save_logs=False,save_dataset=False,base_seed=0,)
+
